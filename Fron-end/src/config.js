ac9cb4ff7b33a5663e4c5d2dfa15d54cdd243a12
Abhinav Anand <abhinav.a@mantralabsglobal.com>
2020-04-18 11:19:30 +0530

    Bug fixes

--- Fron-end/src/config.js
+++ Fron-end/src/config.js
@@ -4,10 +4,9 @@ const API_URL = 'http://localhost:4000';
     module.exports = {
         SIGNIN: API_URL + '/user/login',
         SIGNUP: API_URL + '/user/signip',
-
         CHAT_ROOM: API_URL + '/chat/room',
-
         GROUP_LIST: API_URL + '/chat/member',
+        FETCH_GROUP_MESSAGE: API_URL + '/chat/message/',
     }
 
 }());
